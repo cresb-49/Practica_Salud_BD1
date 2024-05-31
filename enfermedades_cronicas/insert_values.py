e27b324fced8284c167f023332ce8b5d3a27b9d2
cresb-49 <carlosbpac@gmail.com>
2024-05-30 22:19:12 -0600

    Refactor insert_values.py to only insert rows with id_linea >= 54384

--- enfermedades_cronicas/insert_values.py
+++ enfermedades_cronicas/insert_values.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Paths de entrada y salida
 path_entrada = "E:\PROYECTOS\Practica_Salud_BD1\enfermedades_cronicas\genero_datos.csv"
-path_salida = "E:\PROYECTOS\Practica_Salud_BD1\enfermedades_cronicas\genero_datos.sql"
+path_salida = "E:\PROYECTOS\Practica_Salud_BD1\enfermedades_cronicas\genero_datos_54384.sql"
 
 # Cargamos el archivo CSV
 df = pd.read_csv(path_entrada)
@@ -18,11 +18,12 @@ inserts = []
 # Iteramos por cada fila del DataFrame y creamos los inserts
 for index, row in df.iterrows():
     id_linea = row['id_linea']
-    id_genero = row['id_genero']
-    id_rango_edad = row['id_rango_edad']
-    valor = row['valor']
-    insert = f"INSERT INTO valor_enfermedades_cronicas (id_enfermedades_cronicas, id_genero, id_rango_edad, valor) VALUES ({id_linea}, {id_genero}, {id_rango_edad}, {valor});"
-    inserts.append(insert)
+    if int(id_linea) >= 54384:
+        id_genero = row['id_genero']
+        id_rango_edad = row['id_rango_edad']
+        valor = row['valor']
+        insert = f"INSERT INTO valor_enfermedades_cronicas (id_enfermedades_cronicas, id_genero, id_rango_edad, valor) VALUES ({id_linea}, {id_genero}, {id_rango_edad}, {valor});"
+        inserts.append(insert)
 
 # Escribimos los inserts en el archivo de salida
 with open(path_salida, 'w') as file:
